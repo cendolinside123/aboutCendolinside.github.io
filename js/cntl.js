5e6453a771c040115f72dcbea8e60267c95d2771
jan <jan@code.id>
2021-09-30 00:41:46 +0700

    uncompress code, angular cant read it

--- js/cntl.js
+++ js/cntl.js
@@ -1,1 +1,52 @@
-var app=angular.module("allCntl",["ngRoute"]);app.config(function(e,t,l){e.when("/",{title:"/",templateUrl:"pages/home.html"}).when("/AMe",{title:"/AMe",templateUrl:"pages/aboutMe.html"}).when("/404",{title:"/404 LOL what're you expect?",templateUrl:"pages/404.html"}).otherwise({redirectTo:"/404"}),t.html5Mode(!0),t.hashPrefix("!")}),app.controller("headerController",function(e,t,l){var a=t.innerWidth;e.includeDesktopTemplate=!1,e.includeMobileTemplate=!1,a<700?e.includeMobileTemplate=!0:e.includeDesktopTemplate=!0,console.log("PATH:"+t.location.pathname.substring(0,window.location.pathname.indexOf("/",2))),e.selectPage="/",e.updatePage=function(e){"/"==e?l.path("/"):"AMe"==e?l.path("/AMe"):l.path("/404")}}),app.controller("homeController",function(e){});
+var app = angular.module('allCntl', ['ngRoute']);
+app.config(function($routeProvider,$locationProvider,$httpProvider) {
+    $routeProvider
+    .when("/", {
+      title: "/",
+      templateUrl: "pages/home.html"
+    }).when("/AMe", {
+      title: "/AMe",
+      templateUrl: "pages/aboutMe.html"
+    })
+    .when("/404", {
+      title: "/404 LOL what're you expect?",
+      templateUrl: "pages/404.html"
+    }).otherwise({redirectTo: '/404'});
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix("!");
+});
+app.controller('headerController',function($scope,$window,$location) {
+  var screenWidth = $window.innerWidth;
+  $scope.includeDesktopTemplate = false;
+  $scope.includeMobileTemplate = false;
+  if (screenWidth < 700){  
+      $scope.includeMobileTemplate = true;
+  }else{
+      $scope.includeDesktopTemplate = true;
+  }
+  console.log("PATH:"+$window.location.pathname.substring(0, window.location.pathname.indexOf("/",2)));
+  $scope.selectPage = "/"
+  $scope.updatePage = function(selectPage) {
+    if (selectPage == "/") {
+      $location.path("/")
+    } else if (selectPage == "AMe") {
+      $location.path("/AMe")
+    } else {
+      $location.path("/404")
+    }
+ }
+
+})
+app.controller('homeController',function($scope) {})
+// app.controller('ngengTitle',function($scope, $location) {
+//   $scope.title = $location.path()
+// })
+// app.run(function($location, $rootScope, $scope) {
+//   $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+
+//       if (current.hasOwnProperty('$$route')) {
+
+//           $rootScope.title = current.$$route.title;
+//       }
+//   });
+// });
